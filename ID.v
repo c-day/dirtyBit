159004af7b6d9f78a86f83dec71130d6c9d59601
Craig Day <craigday3@gmail.com>
2014-04-26 19:11:01 -0500

    Revert "Revert "tried global flags...close but not quite""
    This reverts commit 0b10f81e40b2fbee0855dd8113de74843dcddaf3.

--- ID.v
+++ ID.v
@@ -8,8 +8,7 @@ module ID (
   i_wrReg,
   i_wrData,
   i_wrEn,
-	i_Z,
-  o_port0,
+	o_port0,
   o_port1,
   o_sext,
   o_instr,
@@ -27,15 +26,16 @@ module ID (
   o_hlt,
   o_wrRegEn, 
 	o_rdReg1En, 
-	o_rdReg2En
+	o_rdReg2En,
+	o_setFlags
 );
 
-  input i_clk, i_nRst, i_wrEn, i_hlt, i_Z;
+  input i_clk, i_nRst, i_wrEn, i_hlt;
   input [3:0] i_wrReg;
   input [15:0] i_wrData, i_instr, i_pc;
   output [15:0] o_port0, o_port1, o_sext, o_instr;
   output [3:0] o_wrReg, o_aluOp, o_shAmt, o_rdReg1, o_rdReg2;
-  output o_mem2reg, o_sawBr, o_sawJ, o_memRd, o_memWr, o_wrRegEn, o_hlt, o_aluSrc, o_rdReg1En, o_rdReg2En;
+  output o_mem2reg, o_sawBr, o_sawJ, o_memRd, o_memWr, o_wrRegEn, o_hlt, o_aluSrc, o_rdReg1En, o_rdReg2En, o_setFlags;
 
 	wire rdEn1, rdEn2;
 
@@ -49,7 +49,7 @@ module ID (
 	assign o_rdReg2En = rdEn2;
 
   control ctrl(.instr(i_instr), .rdEnReg1(rdEn1), .rdEnReg2(rdEn2), .wrRegEn(o_wrRegEn), .rdReg1(o_rdReg1), .rdReg2(o_rdReg2), .wrReg(o_wrReg), .memRd(o_memRd),
-  .memWr(o_memWr), .aluOp(o_aluOp), .shAmt(o_shAmt), .mem2reg(), .sawBr(o_sawBr), .sawJ(o_sawJ), .aluSrc(o_aluSrc), .hlt(o_hlt), .Z(i_Z));
+  .memWr(o_memWr), .aluOp(o_aluOp), .shAmt(o_shAmt), .mem2reg(), .sawBr(o_sawBr), .sawJ(o_sawJ), .aluSrc(o_aluSrc), .hlt(o_hlt), .setFlags(o_setFlags));
 
   rf rf(.clk(i_clk), .p0_addr(o_rdReg1), .p1_addr(o_rdReg2), .p0(o_port0), .p1(o_port1), .re0(rdEn1), .re1(rdEn2), .dst_addr(i_wrReg), .dst(i_wrData), .we(i_wrEn), .hlt(i_hlt));
 
